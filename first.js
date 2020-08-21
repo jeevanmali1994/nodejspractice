16527819d6808560e6a8932894db14cd2d5d0e5f
jeevan <jeevanmali1994@gmail.com>
2020-08-21 12:26:42 +0530

    calling a function as object

--- first.js
+++ first.js
@@ -1,1 +1,12 @@
-demo
+//demo of treatign a function as variable i.e assigning it to a variable
+
+//function whose object is made
+function toBeObject(){
+    console.log("Called a function as object");
+}
+
+function callingTheObject(funct){
+    toBeObject();
+}
+
+callingTheObject();
